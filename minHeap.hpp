f10d08e849b67c99d650e14591ce9efd55d42310
Puskar0304 <puskarbag164@gmail.com>
2026-09-20 19:39:17 +0530

    Initial Commit

--- minHeap.hpp
+++ minHeap.hpp
@@ -1,13 +1,13 @@
 #pragma once
 
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
 
 class minHeap {
     vector<int> arr;
-    int n = arr.size();
 
     void heapifyDown(int idx) {
+        int n = arr.size();
         int smallest = idx;
         int l = 2 * idx + 1;
         int r = 2 * idx + 2;
@@ -23,9 +23,36 @@ class minHeap {
         }
     }
 
+    void heapifyUp(int idx) {
+        if (idx == 0) {
+            return ;
+        }
+        int par = (idx - 1) / 2;
+        if (arr[par] > arr[idx]) {
+            swap(arr[idx], arr[par]);
+            heapifyUp(par);
+        }
+    }
+
     void buildHeap() {
         for (int i = arr.size()/2 -1; i >= 0; i--) {
             heapifyDown(i);
         }
     }
+    public:
+    
+        void insert(int n) {
+            arr.push_back(n);
+            heapifyUp(arr.size() - 1);
+        }
+
+        int extractMin() {
+            int mini = arr[0];
+            arr[0] = arr.back();
+            arr.pop_back();
+            if(!arr.empty()) {
+                heapifyDown(0);
+            }
+            return mini;
+        }
 };
