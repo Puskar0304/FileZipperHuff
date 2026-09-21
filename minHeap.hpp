e0c6ffe6496b68c3f6901347cb925fc6dc1719bf
Puskar0304 <puskarbag164@gmail.com>
2026-09-21 12:36:16 +0530

    Initial Commit

--- minHeap.hpp
+++ minHeap.hpp
@@ -3,18 +3,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+struct Node {
+    char c;
+    int freq;
+    Node* left;
+    Node* right;
+};
+
 class minHeap {
-    vector<int> arr;
+    vector<Node*> arr;
 
     void heapifyDown(int idx) {
         int n = arr.size();
         int smallest = idx;
         int l = 2 * idx + 1;
         int r = 2 * idx + 2;
-        if (l < n && arr[smallest] > arr[l]) {
+        if (l < n && arr[smallest]->freq > arr[l]->freq) {
             smallest = l;
         }
-        if (r < n && arr[smallest] > arr[r]) {
+        if (r < n && arr[smallest]->freq > arr[r]->freq) {
             smallest = r;
         }
         if (smallest != idx) {
@@ -28,7 +35,7 @@ class minHeap {
             return ;
         }
         int par = (idx - 1) / 2;
-        if (arr[par] > arr[idx]) {
+        if (arr[par]->freq > arr[idx]->freq) {
             swap(arr[idx], arr[par]);
             heapifyUp(par);
         }
@@ -41,13 +48,13 @@ class minHeap {
     }
     public:
     
-        void insert(int n) {
+        void insert(Node* n) {
             arr.push_back(n);
             heapifyUp(arr.size() - 1);
         }
 
-        int extractMin() {
-            int mini = arr[0];
+        Node* extractMin() {
+            Node* mini = arr[0];
             arr[0] = arr.back();
             arr.pop_back();
             if(!arr.empty()) {
